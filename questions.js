475cd5aa4d5178b14fcffb5891062e497cdcc13d
Victor CROENNE <victor.croenne16@gmail.com>
2025-11-21 10:06:16 +0100

    avancée1

--- questions.js
+++ questions.js
@@ -1,24 +1,39 @@
 // questions.js
 const questionsBank = [
     {
-        question: "Quelle est la capitale de la France ?",
-        type: "qcm",
-        choices: ["Lyon", "Paris", "Marseille", "Toulouse"],
-        answer: 1,
-        explanation: "Paris est la capitale de la France."
-    },
-    {
-        question: "Le Soleil est une étoile.",
+        question: "La Suisse est, selon le rapport, le pays qui obtient le plus haut score de bonheur ?",
         type: "vf",
         choices: ["Vrai", "Faux"],
         answer: 0,
-        explanation: "Le Soleil est bien une étoile, une naine jaune."
+        explanation: "C’est bien vrai ! Avec un score de 7.587."
     },
     {
-        question: "Quel est le plus grand océan du monde ?",
+        question: "Lequel de ces pays est le plus heureux ?",
         type: "qcm",
-        choices: ["Atlantique", "Arctique", "Indien", "Pacifique"],
+        choices: ["Oman", "Irlande","Australie", "Suède"],
         answer: 3,
-        explanation: "Le Pacifique est le plus vaste océan de la planète."
+        explanation: "Les pays scandinaves sont connues pour respirer la joie de vivre."
+    },
+    {
+        question: "En 2019, la France a accueilli plus de 200 millions de visiteurs",
+        type: "qcm",
+        choices: ["Vrai","Faux"],
+        answer: 0,
+        explanation: "Plus de 217 millions selon notre rapport !"
+    },
+    {
+        question: "Quel pays avait le plus haut taux de chômage en 2019 ?",
+        type: "qcm",
+        choices: ["États-Unis", "Canada"],
+        answer: 1,
+        explanation: "5,69% pour le Canada contre seulement 3,67% pour les États-Unis."
+    },
+    {
+        question: "En 2019, lequel de ces pays avait le plus haut taux d’inflation",
+        type: "qcm",
+        choices: ["Colombie","France","Danemark","Soudan"],
+        answer: 3,
+        explanation: "Il s’agit du Soudan avec plus de 50% d’inflation cette année là"
     }
+    
 ];
